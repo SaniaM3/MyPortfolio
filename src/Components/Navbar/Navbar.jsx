0d7f7f4ff82ded79ae792395b21e50afc4fc2719
Aleksandr Masko <amasko18@gmail.com>
2022-04-08 13:47:16 +0300

    commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import s from './Navbar.module.css'
+
+const Navbar = () => {
+    return (
+        <div className={s.nWrapper}>
+           <div className={s.nLeft}>
+               <div className={s.nName}>Alexander</div>
+               <span>toggle</span>
+           </div>
+           <div className={s.nRight}>
+               <div className={s.nList}>
+                   <ul style={{listStyleType:'none'}}>
+                       <li>Home</li>
+                       <li>Services</li>
+                       <li>Experience</li>
+                       <li>Portfolio</li>
+                       <li>Testimonials</li>
+                   </ul>
+               </div>
+               <button className={s.button}>
+                   Contact us
+               </button>
+           </div>
+        </div>
+    );
+};
+
+export default Navbar;
