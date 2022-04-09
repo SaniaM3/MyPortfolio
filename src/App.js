1deeb9df5ec3b9e643151e79140c4ef18c5c26fd
Aleksandr Masko <amasko18@gmail.com>
2022-04-09 13:20:33 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import './App.css';
-import Exprience from './Components/Exprience/Exprience';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
 import Intro from './Components/Intro/Intro';
 import Navbar from './Components/Navbar/Navbar';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Services from './Components/Services/Services';
-import Testimonial from './Components/Testimonial/Testimonial';
 import Works from './Components/Works/Works';
+import { useContext } from "react";
+import { themeContext } from "./Context";
+// import Exprience from './Components/Exprience/Exprience';
+// import Testimonial from './Components/Testimonial/Testimonial';
 
 function App() {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
   return (
-    <div className="App">
+    <div className="App" style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}>
       <Navbar />
       <Intro />
       <Services />
-      <Exprience />
       <Works />
       <Portfolio />
+      <Contact />
+      <Footer />
     </div>
   );
 }
